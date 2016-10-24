fa160f4b9f9582c75b5089453d7624ee5a18bf2c
egerr10 <egerr10@gmail.com>
2016-10-24 09:37:28 +0500

    upd

--- controller.js
+++ controller.js
@@ -1,3 +1,37 @@
-/**
- * Created by egerr10 on 23.10.2016.
+
+var phonecatApp = angular.module('phonecatApp', []);
+phonecatApp.controller('PhoneListCtrl', function($scope) {
+    $scope.title = 'Телефоны';
+    $scope.phones = [
+        {name: 'Nexus S',
+            snipet: 'Fast but got faster with Nexus S'},
+        {name:'Motorola XOOM with Wi-Fi.',
+            snipet: 'The Next, Next Generation Tablet.'},
+        {name:'Motorola XOOM',
+            snipet:'The next, next generation tablet'}
+    ];
+
+    //Filter
+
+    var date = new Date();
+    $scope.today = date;
+});
+
+
+/*function Todo ($scope) {
+ $scope.totalTodos = 4;
+ $scope.todo2 = 'its good!';
+
+ $scope.todos = [
+ {text:'Hellow Worlds', done:false},
+ {text:'Build an app', done:false}
+ ];
+
+ $scope.action = function () {
+ $scope.ex = 'OK'
+ }
+ $scope.ex = 'World';
+
+ $scope.shito = 'shito';
+ }
  */
