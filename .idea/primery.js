0e25470c02d42ae9456c0211b4139ee18e879a99
egerr10 <egerr10@gmail.com>
2016-10-26 19:28:06 +0500

    upd

---
+++ .idea/primery.js
@@ -0,0 +1,64 @@
+/**
+ * Created by Ivanov_es on 26.10.2016.
+ */
+
+//для примеров
+
+
+/*function Todo ($scope) {
+ $scope.totalTodos = 4;
+ $scope.todo2 = 'its good!';
+
+ $scope.todos = [
+ {text:'Hellow Worlds', done:false},
+ {text:'Build an app', done:false}
+ ];
+
+ $scope.action = function () {
+ $scope.ex = 'OK'
+ }
+ $scope.ex = 'World';
+
+ $scope.shito = 'shito';
+ }
+
+ //конструктор
+ function User(name, surname, age) {
+
+ this.name = name;
+ this.surname = surname;
+ this.age = age;
+
+ this.sayHi = function() {
+ console.log('Моё имя: ' + this.name + ' ' + this.surname + ' ' + this.age);
+ };
+
+ this.printPage = function () {
+ $scope.printThis = 'Моё имя: ' + this.name + ' ' + this.surname;
+ $scope.printAge = this.age;
+ }
+
+ }
+
+ var ivan = new User('Иван', 'Петров', '25');
+ ivan.sayHi();
+ ivan.printPage();
+
+ var ivan2 = new User('Иван', 'Петров', '26');
+ ivan2.sayHi();
+ ivan2.printPage();
+
+ var ivan3 = new User('Иван', 'Петров', '27');
+ ivan3.sayHi();
+
+ ////////////
+ фильтр
+
+ $scope.doneAndFilter = function (phoneItem) {
+ return phoneItem.name && phoneItem.priority > 1 && phoneItem.status === true;
+ }
+
+ <p>Общее кол-во телефонов {{(phones | filter:doneAndFilter).length}}</p>
+
+ //////////
+ */
--- controller.js
+++ controller.js
@@ -5,16 +5,22 @@ phonecatApp.controller('PhoneListCtrl', function($scope) {
     $scope.phones = [
         {'name': 'Nexus S',
             'snipet': 'Fast but got faster with Nexus S',
-        'status': true},
+        'status': true,
+        'priority':1},
         {'name':'Motorola XOOM with Wi-Fi.',
             'snipet': 'The Next, Next Generation Tablet.',
-            'status': false},
+            'status': false,
+            'priority':2},
         {'name':'Motorola XOOM',
             'snipet':'The next, next generation tablet',
-            'status': true}
+            'status': true,
+            'priority':3}
     ];
 
     //Filter
+$scope.doneAndFilter = function (phoneItem) {
+    return phoneItem.name && phoneItem.priority > 1 && phoneItem.status === true;
+}
 
     var date = new Date();
     var date2 = angular.isDate(date);
@@ -24,51 +30,3 @@ phonecatApp.controller('PhoneListCtrl', function($scope) {
     }
 
 });
-
-
-/*function Todo ($scope) {
- $scope.totalTodos = 4;
- $scope.todo2 = 'its good!';
-
- $scope.todos = [
- {text:'Hellow Worlds', done:false},
- {text:'Build an app', done:false}
- ];
-
- $scope.action = function () {
- $scope.ex = 'OK'
- }
- $scope.ex = 'World';
-
- $scope.shito = 'shito';
- }
-
-//конструктор
- function User(name, surname, age) {
-
- this.name = name;
- this.surname = surname;
- this.age = age;
-
- this.sayHi = function() {
- console.log('Моё имя: ' + this.name + ' ' + this.surname + ' ' + this.age);
- };
-
- this.printPage = function () {
- $scope.printThis = 'Моё имя: ' + this.name + ' ' + this.surname;
- $scope.printAge = this.age;
- }
-
- }
-
- var ivan = new User('Иван', 'Петров', '25');
- ivan.sayHi();
- ivan.printPage();
-
- var ivan2 = new User('Иван', 'Петров', '26');
- ivan2.sayHi();
- ivan2.printPage();
-
- var ivan3 = new User('Иван', 'Петров', '27');
- ivan3.sayHi();
- */
